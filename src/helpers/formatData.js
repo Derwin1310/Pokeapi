a49d42ebcd6f146919ea73bb3afb5e1d3710ac76
Derwin Romero <derwin1310@gmail.com>
2022-04-21 01:26:30 -0400

    feature/option-buttons 02: adding the paginationBar consts to helpers for better readability

--- src/helpers/formatData.js
+++ src/helpers/formatData.js
@@ -7,3 +7,15 @@ export function formatWeight(weight) {
 	const weightInTons = weightInKgs / 1000;
 	return	weightInKgs < 1000 ? `${weightInKgs} kg` : `${weightInTons} ton`;
 }
+
+export function getActualPage(pageNumbers) {
+	return Math.ceil(pageNumbers / 10) + 1;
+}
+
+export function getTotalPages(totalOfPokemons) {	
+	return Math.ceil(totalOfPokemons / 10) + 1;
+}
+
+export function getLastPokemons(totalOfPokemons) {
+	return parseInt(totalOfPokemons / 10, 10) * 10 + 10;
+}
